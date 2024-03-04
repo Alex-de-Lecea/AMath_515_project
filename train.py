9c636b9911e19921c42765372e6ca7fe5151f63d
Alex-de-Lecea <100385375+Alex-de-Lecea@users.noreply.github.com>
2024-03-04 09:51:11 -0800

    Some Initial Files

---
+++ train.py
@@ -0,0 +1,12 @@
+import os
+import torch 
+import torchvision
+import torch.nn as nn
+import torchvision.transforms as transforms
+import torch.optim as optim
+import matplotlib.pyplot as plt
+import torch.nn.functional as F
+ 
+from torchvision import datasets
+from torch.utils.data import DataLoader
+from torchvision.utils import save_image
